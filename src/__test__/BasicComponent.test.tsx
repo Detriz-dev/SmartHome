9ce85516c2b5a813f03dbcf60a8c9c3e1763c9d4
dawidkowalczyk <kowalczyk.dawid@gmail.com>
2025-11-16 17:29:40 +0100

    Remove. package-lock

--- src/__test__/BasicComponent.test.tsx
+++ src/__test__/BasicComponent.test.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+ 
 import { render, screen, fireEvent, cleanup } from '@testing-library/react';
-import { describe, test, expect, beforeEach, afterEach, vi } from 'vitest';
+import { describe, test, expect, beforeEach, afterEach } from 'vitest';
 import '@testing-library/jest-dom';
 import { BasicComponent } from '../components/BasicComponent';
 
